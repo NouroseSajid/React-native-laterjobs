c3751fd036166eba27ddef31edb25a020f287ddb
NouroseSajid <nourosesajid@gmail.com>
2025-02-13 07:53:47 +0000

    Empty screens files

---
+++ src/screens/HomeScreen/HomeScreen.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {View, Text} from "react-native"
+
+
+const HomeScreen = () => {
+
+
+return (
+    <View>
+        <Text>
+            "Homescreen text, this is the shomescreen"
+        </Text>
+    </View>
+)
+}
+
+export default HomeScreen;
